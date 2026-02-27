2406590f4b8d318609d5aba0a7a4524cd466ebac
Melissa Paquette <melissapaquette1206@gmail.com>
2026-02-27 13:47:55 -0500

    Header, Modal with form, Weather card, Item card

---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,32 @@
+import "./ModalWithForm.css";
+
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
+  return (
+    <div className={'modal ${atciveModal === "add garment" && "modal__opened}'}>
+      <div className="modal__content">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close"
+        >
+          CLOSE
+        </button>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ModalWithForm;
